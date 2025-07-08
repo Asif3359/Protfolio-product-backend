a6a95be0ef0482111ba43b914c3e72b8cc36eb57
Asif Ahammed <asifahammed359@gmail.com>
2025-07-09 00:42:46 +0600

    Migrate to pug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.listen(port, () => {
 app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
